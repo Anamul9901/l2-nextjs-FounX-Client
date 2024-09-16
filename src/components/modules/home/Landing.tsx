d728f934344dfe1e15da9931b7d2808ea6a1c14b
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-17 00:05:39 +0600

    Home page module

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,11 +1,27 @@
 import { Input } from "@nextui-org/input";
+import { SearchIcon } from "../../icons";
 
 const Landing = () => {
-    return (
-        <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
-        <Input />
+  return (
+    <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
+      <div className="pt-32 max-w-xl flex-1 mx-auto">
+        <form className="" action="" method="">
+          <Input
+            aria-label="Search"
+            classNames={{
+              inputWrapper: "bg-default-100",
+              input: "text-sm"
+            }}
+            placeholder="Search..."
+            size="lg"
+            startContent={
+              <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-100"/>
+            }
+          />
+        </form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Landing;
--- src/components/modules/home/RecentPost.tsx
+++ src/components/modules/home/RecentPost.tsx
@@ -1,10 +1,31 @@
+import { Button } from "@nextui-org/button";
+import Container from "../../UI/Container";
+import Link from "next/link";
+import { getRecentPosts } from "@/src/services/RecentPosts";
 
-const RecentPost = () => {
-    return (
-        <div>
-            REcent post
-        </div>
-    );
+const RecentPost = async () => {
+  const { data: posts } = await getRecentPosts();
+  console.log("data", posts);
+  return (
+    <Container>
+      <div className="section-title my-8">
+        <h2 className="mb-2 text-center text-2xl">Recent Found Items</h2>
+        <p className="text-center">
+          A lst of items that have been recently found and reported.
+        </p>
+      </div>
+      <div className="my-8 grid justify-center gap-10 sm:grid-cols-1 md:grid-cols-4">
+        {posts.map((item) => (
+          <p>{item.title}</p>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <Button className="rounded-md bg-default-900 text-default">
+          <Link href="/foun-items">See All</Link>
+        </Button>
+      </div>
+    </Container>
+  );
 };
 
 export default RecentPost;
