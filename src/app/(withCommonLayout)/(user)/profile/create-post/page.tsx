3502c0376dc294ad32cdfbde7db0eef68bcd08bc
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-22 18:34:53 +0600

    useFieldArray use for dynamic field

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,10 +1,37 @@
+"use client";
+import FXInput from "@/src/components/form/FXInput";
+import { Button } from "@nextui-org/button";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
 
 const CratePost = () => {
-    return (
-        <div>
-            <h1>Create post page</h1>
-        </div>
-    );
+  const methods = useForm();
+  const { control, handleSubmit } = methods;
+
+  //! useFieldArray er maddome eksathe onk field add kora jabe.
+  const {} = useFieldArray({
+    control,
+    name: "questions",
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title" />
+          <Button type="submit">Post</Button>
+        </form>
+      </FormProvider>
+    </div>
+  );
 };
 
 export default CratePost;
