f2445ca58fde958b35196ea4da6f864d8c4a83b7
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-22 18:52:12 +0600

    Dynamic field added

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import FXInput from "@/src/components/form/FXInput";
 import { Button } from "@nextui-org/button";
+import { Divider } from "@nextui-org/divider";
 import {
   FieldValues,
   FormProvider,
@@ -13,8 +14,8 @@ const CratePost = () => {
   const methods = useForm();
   const { control, handleSubmit } = methods;
 
-  //! useFieldArray er maddome eksathe onk field add kora jabe.
-  const {} = useFieldArray({
+  //! useFieldArray er maddome eksathe onk field add kora jabe.(react-hook-from er)
+  const { fields, append, remove } = useFieldArray({
     control,
     name: "questions",
   });
@@ -22,11 +23,31 @@ const CratePost = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
+
+  const handledFieldAppend = () => {
+    append({ name: "questions" });
+  };
   return (
     <div>
       <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FXInput name="title" label="Title" />
+
+          <Divider className="my-5" />
+
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Owner verification question</h1>
+            <Button onClick={() => handledFieldAppend()}>Append</Button>
+          </div>
+
+          {fields.map((field, index) => (
+            <div key={field.id}>
+              <FXInput name={`questions.${index}.value`} label="Questions" />
+            </div>
+          ))}
+
+          <Divider className="my-5" />
+
           <Button type="submit">Post</Button>
         </form>
       </FormProvider>
